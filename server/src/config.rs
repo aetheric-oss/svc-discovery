2eba331291d77ba247cf11c23e70ee728f50fc01
A.M. Smith <smitham42@gmail.com>
2024-03-03 07:27:57 -0800

    feat: add grpc client for gis service

--- server/src/config.rs
+++ server/src/config.rs
@@ -16,6 +16,10 @@ pub struct Config {
     pub docker_port_rest: u16,
     /// path to log configuration YAML file
     pub log_config: String,
+    /// host for the gis gRPC server
+    pub gis_host_grpc: String,
+    /// port for the gis gRPC server
+    pub gis_port_grpc: u16,
 }
 
 impl Default for Config {
@@ -31,18 +35,23 @@ impl Config {
             docker_port_grpc: 50051,
             docker_port_rest: 8000,
             log_config: String::from("log4rs.yaml"),
+            gis_host_grpc: String::from("localhost"),
+            gis_port_grpc: 50052,
         }
     }
 
     /// Create a new `Config` object using environment variables
-    pub fn from_env() -> Result<Self, ConfigError> {
+    pub fn try_from_env() -> Result<Self, ConfigError> {
         // read .env file if present
         dotenv().ok();
+        let default_config = Config::default();
 
         config::Config::builder()
-            .set_default("docker_port_grpc", 50051)?
-            .set_default("docker_port_rest", 8000)?
-            .set_default("log_config", String::from("log4rs.yaml"))?
+            .set_default("docker_port_grpc", default_config.docker_port_grpc)?
+            .set_default("docker_port_rest", default_config.docker_port_rest)?
+            .set_default("log_config", default_config.log_config)?
+            .set_default("gis_host_grpc", default_config.gis_host_grpc)?
+            .set_default("gis_port_grpc", default_config.gis_port_grpc)?
             .add_source(Environment::default().separator("__"))
             .build()?
             .try_deserialize()
